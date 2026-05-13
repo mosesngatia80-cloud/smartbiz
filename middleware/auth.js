ec60597918bec747ccf47cc03f044fc4736af65d
Moses Ngatia <mosesngatia80@gmail.com>
2026-05-13 15:12:43 +0300

    Fix auth, orders and expense APIs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,51 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-  console.log("🔥 AUTH BYPASSED");
 
-  req.user = {
-    user: "DEV_USER",
-    _id: "DEV_USER",
-    business: "DEV_USER"
-  };
+  try {
 
-  next();
+    const authHeader =
+      req.headers.authorization;
+
+    if (!authHeader) {
+
+      return res.status(401).json({
+        message: "No token provided"
+      });
+    }
+
+    const token =
+      authHeader.split(" ")[1];
+
+    if (!token) {
+
+      return res.status(401).json({
+        message: "Invalid token"
+      });
+    }
+
+    const decoded =
+      jwt.verify(
+
+        token,
+
+        process.env.JWT_SECRET ||
+        "smartbiz_secret"
+      );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (err) {
+
+    console.error(
+      "AUTH ERROR:",
+      err.message
+    );
+
+    res.status(401).json({
+      message: "Unauthorized"
+    });
+  }
 };
