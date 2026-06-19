b5b48f47156c8d73437aafe3ea4a728ea74a833f
Moses Ngatia <mosesngatia80@gmail.com>
2026-06-19 20:32:18 +0300

    Add WhatsApp subscription lookup

--- middleware/checkSubscription.js
+++ middleware/checkSubscription.js
@@ -1,15 +1,36 @@
 const Subscription =
   require("../models/Subscription");
 
+const Business =
+  require("../models/Business");
+
 module.exports =
   async (req, res, next) => {
 
     try {
 
-      const businessId =
+      let businessId =
         req.business?._id ||
         req.body.business ||
         req.query.business;
+
+      if (
+        !businessId &&
+        req.query.whatsappNumber
+      ) {
+
+        const business =
+          await Business.findOne({
+            whatsappNumber:
+              req.query.whatsappNumber
+          });
+
+        if (business) {
+          businessId =
+            business._id;
+        }
+
+      }
 
       if (!businessId) {
         return next();
