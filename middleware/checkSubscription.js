6a7f731fea4f0f0473909f879a4afb0f54186614
Moses Ngatia <mosesngatia80@gmail.com>
2026-06-25 10:12:40 +0300

    Disable subscription enforcement during beta

--- middleware/checkSubscription.js
+++ middleware/checkSubscription.js
@@ -32,66 +32,21 @@ module.exports =
 
       }
 
-      if (!businessId) {
-        return next();
-      }
+      /* =========================
+         BETA MODE
+         DO NOT BLOCK ANY ACCOUNT
+      ========================= */
 
-      const subscription =
-        await Subscription.findOne({
-          business: businessId
-        });
-
-      if (!subscription) {
-        return res.status(403).json({
-          success: false,
-          message:
-            "No active subscription"
-        });
-      }
-
-      const now = new Date();
-
-      if (
-        subscription.expiryDate &&
-        now > subscription.expiryDate &&
-        now <= subscription.graceUntil
-      ) {
-
-        subscription.status =
-          "GRACE";
-
-        await subscription.save();
-
-        req.subscriptionWarning =
-          "Subscription expired. Grace period active.";
-
-        return next();
-      }
-
-      if (
-        subscription.graceUntil &&
-        now > subscription.graceUntil
-      ) {
-
-        subscription.status =
-          "SUSPENDED";
-
-        await subscription.save();
-
-        return res.status(403).json({
-          success: false,
-          message:
-            "Subscription expired. Please renew."
-        });
-      }
-
-      next();
+      return next();
 
     } catch (err) {
 
-      res.status(500).json({
-        message: err.message
-      });
+      console.error(
+        "Subscription middleware:",
+        err
+      );
+
+      return next();
 
     }
 
